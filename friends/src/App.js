f5628fbbb858e7fa2d937c3ad456939994c7f3c2
Ash <ashley.cruz.crespo@gmail.com>
2019-05-14 20:39:10 -0400

    created components

--- friends/src/App.js
+++ friends/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ashley's page 
         </a>
       </header>
     </div>
