0818d2ee595a03b229387405854622e4841bedde
Ash <ashley.cruz.crespo@gmail.com>
2019-05-15 08:50:49 -0400

    trying to understand my errors

--- friends/src/App.js
+++ friends/src/App.js
@@ -27,8 +27,54 @@ class App extends React.Component{
       })
   }
 
-  
+  addFriend = newFriend => {
+    axios
+    .post('http://localhost:5000/friends', newFriend)
+    .then(res => {
+      this.setState({ friends: res.data })
+    })
+    .catch(err => console.log(err))
+  }
 
+  deleteFriend = (id) => {
+    axios
+    .delete(`http://localhost:5000/friends/${id}`)
+    .then(res => {
+      this.setState({ friends: res.data })
+    })
+    .catch(err => console.log(err))
+  }
 
+  updateFriend = (updatedFriend, id) => {
+    axios
+    .put(`http://localhost:5000/friends/${id}`, updatedFriend)
+    .then(res => {
+      this.setState({ friends: res.data })
+    })
+    .catch(err => console.log(err))
+  }
 
+    render(){
+    return (
+      <div className="App">
+      <div className="header-container">
+        <h1>Friends</h1>
+        <FriendForm
+        addFriend={this.addFriend}
+        />
+        </div>
+
+        <div className="all-my-friends">
+          <Friends
+          friends={this.state.friends}
+          deleteFriend={this.deleteFriend}
+          updateFriend={this.updateFriend}
+          />
+        </div>
+
+      </div>
+    );
+  }
 }
+
+export default App;
